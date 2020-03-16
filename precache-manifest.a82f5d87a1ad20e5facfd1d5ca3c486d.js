7af24e066e005e7020ff1c171807106ef85d183f
Mohammad <mohammad.m@live.ca>
2020-03-15 23:35:59 -0400

    Updates

---
+++ precache-manifest.a82f5d87a1ad20e5facfd1d5ca3c486d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92351c3ddef8e64765f11d8f8e841dc7",
+    "url": "/react-gh/index.html"
+  },
+  {
+    "revision": "2aaf8957c087771596f8",
+    "url": "/react-gh/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "56a97169d06ea0064592",
+    "url": "/react-gh/static/js/2.3261ab3b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-gh/static/js/2.3261ab3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2aaf8957c087771596f8",
+    "url": "/react-gh/static/js/main.25f2cfcc.chunk.js"
+  },
+  {
+    "revision": "ceece10d16bbe8e509eb",
+    "url": "/react-gh/static/js/runtime-main.6f916acd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh/static/media/logo.5d5d9eef.svg"
+  }
+]);
