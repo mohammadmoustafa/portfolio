602ef861be986bc03884ecf52471f0b50a86bf15
Mohammad <mohammad.m@live.ca>
2020-03-17 17:14:36 -0400

    Revert "Base style is present"
    This reverts commit 487ef360ce2bec61aac70b172eb4590b333de0a6.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Education from './components/Education';
-import ExperienceSection from './components/ExperienceSection';
-import AppWindow from './components/AppWindow';
-
-// <Education school="University of Toronto" gradYear="2020" program="Computer Science" />
-// <img src={logo} className="App-logo" alt="logo" />
 
 function App() {
   return (
     <div className="App">
-      <AppWindow />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,12 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Education from './components/Education';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-//ReactDOM.render(<Education school="University of Toronto" gradYear="2020" program="Computer Science" />,
-//	document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
